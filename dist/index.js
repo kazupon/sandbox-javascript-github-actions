31fa10f9448f0139f191a92a7344067feb657293
kazuya kawaguchi <kawakazu80@gmail.com>
2020-01-10 17:47:25 +0900

    updates

--- dist/index.js
+++ dist/index.js
@@ -497,27 +497,36 @@ module.exports = require("os");
 const core = __webpack_require__(470)
 const { context, GitHub } = __webpack_require__(469)
 
-(async () => {
-  try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet')
-    console.log(`Hello ${nameToGreet}!`)
-    const token = core.getInput('github-token', {required: true})
-    const labels = core.getInput('labels').split(',')
-    const opts = {}
-    const client = new GitHub(token, opts)
-    const issues = await client.issues.list({ labels })
-    console.log('issues count', issues.count)
-    const time = (new Date()).toTimeString()
-    core.setOutput("time", time)
-    core.setOutput("count", issues.count)
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
-  } catch (error) {
-    core.setFailed(error.message)
+process.on('unhandledRejection', handleError)
+main().catch(handleError)
+
+async function main () {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet')
+  console.log(`Hello ${nameToGreet}!`)
+  const token = core.getInput('github-token', {required: true})
+  const labels = core.getInput('labels').split(',')
+  const opts = {}
+  const client = new GitHub(token, opts)
+  const issues = await client.issues.list({ labels })
+  console.log('issues count', issues.count)
+  const time = (new Date()).toTimeString()
+  core.setOutput("time", time)
+  core.setOutput("count", issues.count)
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
+}
+
+function handleError(err) {
+  console.error(err)
+  if (err && err.message) {
+    core.setFailed(err.message)
+  } else {
+    core.setFailed(`Unhandled error: ${err}`)
   }
-})()
+}
+
 
 /***/ }),
 
