21eb4321fc8d04b8a74d7b18ddd03a83b26b8b68
kazuya kawaguchi <kawakazu80@gmail.com>
2020-01-10 17:57:10 +0900

    update

--- dist/index.js
+++ dist/index.js
@@ -504,18 +504,24 @@ async function main () {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet')
   console.log(`Hello ${nameToGreet}!`)
+  // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`)
+  console.log('context.owner', context.owner)
+  console.log('context.repo', context.repo)
   const token = core.getInput('github-token', {required: true})
   const labels = core.getInput('labels').split(',')
   const opts = {}
   const client = new GitHub(token, opts)
-  const issues = await client.issues.list({ labels })
+  const issues = await client.issues.listForRepo({
+    labels,
+    owner: context.owner,
+    repo: context.repo,
+  })
   console.log('issues count', issues.count)
   const time = (new Date()).toTimeString()
   core.setOutput("time", time)
   core.setOutput("count", issues.count)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
 }
 
 function handleError(err) {
