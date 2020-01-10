940e967fd3871ea45a5f9886c7e0bd50fdf8eeeb
kazuya kawaguchi <kawakazu80@gmail.com>
2020-01-10 17:26:09 +0900

    update

--- dist/index.js
+++ dist/index.js
@@ -494,28 +494,30 @@ module.exports = require("os");
 /***/ 104:
 /***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
-const core = __webpack_require__(470);
-const { context, GitHub } = __webpack_require__(469);
+const core = __webpack_require__(470)
+const { context, GitHub } = __webpack_require__(469)
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const token = core.getInput('github-token', {required: true})
-  const labels = core.getInput('previews').split(',')
-  const opts = {}
-  const client = new GitHub(token, opts)
-  const issues = client.issues.list({ labels })
-  console.log('issues count', issues.count)
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  core.setOutput("count", issues.count);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+(async () => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet')
+    console.log(`Hello ${nameToGreet}!`)
+    const token = core.getInput('github-token', {required: true})
+    const labels = core.getInput('previews').split(',')
+    const opts = {}
+    const client = new GitHub(token, opts)
+    const issues = await client.issues.list({ labels })
+    console.log('issues count', issues.count)
+    const time = (new Date()).toTimeString()
+    core.setOutput("time", time)
+    core.setOutput("count", issues.count)
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+})()
 
 /***/ }),
 
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const core = require('@actions/core');
-const { context, GitHub } = require('@actions/github');
+const core = require('@actions/core')
+const { context, GitHub } = require('@actions/github')
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const token = core.getInput('github-token', {required: true})
-  const labels = core.getInput('previews').split(',')
-  const opts = {}
-  const client = new GitHub(token, opts)
-  const issues = client.issues.list({ labels })
-  console.log('issues count', issues.count)
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  core.setOutput("count", issues.count);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+(async () => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet')
+    console.log(`Hello ${nameToGreet}!`)
+    const token = core.getInput('github-token', {required: true})
+    const labels = core.getInput('previews').split(',')
+    const opts = {}
+    const client = new GitHub(token, opts)
+    const issues = await client.issues.list({ labels })
+    console.log('issues count', issues.count)
+    const time = (new Date()).toTimeString()
+    core.setOutput("time", time)
+    core.setOutput("count", issues.count)
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+})()
